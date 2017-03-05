b8553627200bf927b90449d5d2390835a08757ec
Irina <italia2505@gmail.com>
2017-03-05 19:55:31 +0200

    responsive, slider

---
+++ js/script.js
@@ -0,0 +1,19 @@
+﻿$(function(){
+  var swiper = new Swiper('.swiper-container');
+  $('.swiper-container').Swiper()
+
+});
+
+
+$(function(){
+   if($(window).width() > '840') {
+  $('.bxslider').bxSlider(
+      {
+       pager:false
+      }
+    )
+}
+});
+
+
+ 
