943b912fc66664b6f617b48e9087fc41e7287cc3
Irina <italia2505@gmail.com>
2017-03-13 12:08:56 +0200

    nav

--- js/script.js
+++ js/script.js
@@ -20,6 +20,11 @@ $(function(){
 
 
 $(function(){
+
+  $('.navbar-toggle').click(function(e){
+  e.preventDefault();
+  })
+
   $('.navbar-toggle').on('click', function(){
     var target = $(this).attr('href');
     $(target).addClass('collapse');
@@ -29,3 +34,4 @@ $(function(){
 
 
  
+  
